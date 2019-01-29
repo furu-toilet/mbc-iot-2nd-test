8916f528d13333e55646e269d7535be3278bd7d7
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-01-29 09:55:53 +0900

    Update ShowTenki.js

--- js/ShowTenki.js
+++ js/ShowTenki.js
@@ -1,11 +1,11 @@
 $.ajax({
-  url: "http://api.openweathermap.org/data/2.5/weather?q=takasaki,jp&appid=59ca09e691c813495a3bc5cdd62ad209",
+  url: "https://api.openweathermap.org/data/2.5/weather?q=takasaki,jp&appid=59ca09e691c813495a3bc5cdd62ad209",
   cache: false,
   success:function (weatherdata){
     // img insert
 
     var img = document.createElement('img');
-    img.src = "http://openweathermap.org/img/w/"+weatherdata.weather[0].icon+".png";
+    img.src = "https://openweathermap.org/img/w/"+weatherdata.weather[0].icon+".png";
     img.alt = weatherdata.weather[0].main;
     document.getElementById('icon').appendChild(img);
 
