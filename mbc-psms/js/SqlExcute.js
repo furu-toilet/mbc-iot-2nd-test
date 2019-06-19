96822b72482066b211cf9433fa8ba839a46339f2
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-06-19 17:38:27 +0900

    Update SqlExcute.js

--- mbc-psms/js/SqlExcute.js
+++ mbc-psms/js/SqlExcute.js
@@ -53,7 +53,7 @@ function makeTable(data,tableId){
     for(let i in data){
         rows.push(table.insertRow(1));  // 行の追加
         for(let j in data[i]){
-            cell=rows[i].insertCell(-1);
+            cell=rows[i+1].insertCell(-1);
             cell.appendChild(document.createTextNode(data[i][j]));
             // 背景色の設定
 
