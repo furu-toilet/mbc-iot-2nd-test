ecc94025795f54a9b1f4cbfe8a6b12fcccf1090a
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-06-19 16:15:07 +0900

    Update SqlExcute.js

--- mbc-psms/js/SqlExcute.js
+++ mbc-psms/js/SqlExcute.js
@@ -51,8 +51,8 @@ function makeTable(data,tableId){
     for(let i in data){
         rows.push(table.insertRow(-1));      //行の追加　※縦方向
         for(let j in data[j]){
-            cell = rows[[i]].insertCell(-1);    //行に対して項目を追加　※横方向
-            cell.appendChild(document.createTextNode(data[[i][j]]));   //データの追加
+            cell = rows[i].insertCell(-1);    //行に対して項目を追加　※横方向
+            cell.appendChild(document.createTextNode(data[i][j]));   //データの追加
         }    
     }
     //指定したdiv要素に表を加える
