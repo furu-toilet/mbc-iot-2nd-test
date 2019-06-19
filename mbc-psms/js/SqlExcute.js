bf59964d7abd2ff8c5375ebfb95d9c3330b1d3b0
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-06-19 17:35:35 +0900

    Update SqlExcute.js

--- mbc-psms/js/SqlExcute.js
+++ mbc-psms/js/SqlExcute.js
@@ -42,8 +42,8 @@ function makeTable(data,tableId){
         for(let y in data){
             cell = rows[x].insertCell(-1);
             cell.appendChild(document.createTextNode(y));
+            cell.style.backgroundColor = "#1727ea"; // ヘッダ行
         }
-        cell.style.backgroundColor = "#1727ea"; // ヘッダ行
         break;
     }
     //指定したdiv要素に表を加える
@@ -51,7 +51,7 @@ function makeTable(data,tableId){
     
     //表に2次元配列の要素を格納
     for(let i in data){
-        rows.push(table.insertRow(-1));  // 行の追加
+        rows.push(table.insertRow(0));  // 行の追加
         for(let j in data[i]){
             cell=rows[i].insertCell(-1);
             cell.appendChild(document.createTextNode(data[i][j]));
