1accca2298ab564440f270dea9e3a6119a2bd016
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-06-19 16:18:55 +0900

    Update SqlExcute.js

--- mbc-psms/js/SqlExcute.js
+++ mbc-psms/js/SqlExcute.js
@@ -49,10 +49,10 @@ function makeTable(data,tableId){
     
     //表に2次元配列の要素を格納
     for(let i in data){
-        rows.push(table.insertRow(-1));      //行の追加　※縦方向
+        rows.push(table.insertRow(-1));  // 行の追加
         for(let j in data[i]){
-            cell = rows[i].insertCell(-1);    //行に対して項目を追加　※横方向
-            cell.appendChild(document.createTextNode(data[i][j]));   //データの追加
+            cell=rows[i].insertCell(-1);
+            cell.appendChild(document.createTextNode(data[i][j]));
         }    
     }
     //指定したdiv要素に表を加える
