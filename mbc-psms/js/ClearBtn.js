7bfb133984d03081c54da07562ef0cebcbbd6279
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-06-20 14:41:57 +0900

    Update ClearBtn.js

--- mbc-psms/js/ClearBtn.js
+++ mbc-psms/js/ClearBtn.js
@@ -2,5 +2,7 @@ $(function(){
     // リセットボタンクリック
     $('#sql_reset').on('click',function(){
         document.getElementById("log").value = "";
+        document.getElementById("msg").value = "";
+        document.getElementById("sql").value = "";
     });
 });
