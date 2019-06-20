00c5f225ec6dbfed47f332af4fe1f30bfe8c4f3d
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-06-20 13:22:27 +0900

    Create ErrMsg.php

---
+++ mbc-psms/php/ErrMsg.php
@@ -0,0 +1,24 @@
+<?php
+//Ajaxにて実行でSQL事項結果を2次元連想配列にて返す。
+//$data内に配置
+require_once "./Common.php";
+$db = new Common();
+//header('Content-type: text/plain; charset= UTF-8');
+if(isset($_POST['sql'])){
+    $sql = $_POST['sql'];
+    $data = $db->db_sql($sql);
+    
+    if($data == null){
+        //データなしの場合
+        $data = ["実行結果"=>["該当データなし"]];
+    }else if($db->db_msg() != null){
+        echo json_encode( $db->db_msg() );
+    }else{
+        //正常終了時
+        echo json_encode( $data );
+    }
+    //echo json_encode( $data );
+}else{
+    echo 'FAIL TO AJAX REQUEST';
+}
+?>
