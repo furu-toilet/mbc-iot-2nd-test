fe51d09e38979236749b438a082f8bea298fced6
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-06-20 13:23:35 +0900

    Update ErrMsg.php

--- mbc-psms/php/ErrMsg.php
+++ mbc-psms/php/ErrMsg.php
@@ -6,18 +6,9 @@ $db = new Common();
 //header('Content-type: text/plain; charset= UTF-8');
 if(isset($_POST['sql'])){
     $sql = $_POST['sql'];
-    $data = $db->db_sql($sql);
+    $db->db_sql($sql);
     
-    if($data == null){
-        //データなしの場合
-        $data = ["実行結果"=>["該当データなし"]];
-    }else if($db->db_msg() != null){
-        echo json_encode( $db->db_msg() );
-    }else{
-        //正常終了時
-        echo json_encode( $data );
-    }
-    //echo json_encode( $data );
+    echo json_encode( $db->db_msg() );
 }else{
     echo 'FAIL TO AJAX REQUEST';
 }
