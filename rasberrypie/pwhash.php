4acf8189d20ae33832ea5ef03c77bf7abb9923f0
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-06-26 14:58:06 +0900

    Update pwhash.php

--- rasberrypie/pwhash.php
+++ rasberrypie/pwhash.php
@@ -1,7 +1,7 @@
 <?php
-$key = "mbctoilet";
 $pass = $_GET['pass'];
-$encpass = openssl_encrypt($pass,'AES-128-ECB',$key);
-$decpass = openssl_decrypt($encpass,'AES-128-ECB',$key);
-var_dump($encpass,$decpass);
+
+$hash = password_hash($password, PASSWORD_BCRYPT);
+
+var_dump($hash);
 ?>
