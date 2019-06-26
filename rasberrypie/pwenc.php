38b3ba00f1a2372e6f39beea964e416fc46b1d3c
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-06-26 14:43:19 +0900

    Update pwenc.php

--- rasberrypie/pwenc.php
+++ rasberrypie/pwenc.php
@@ -3,6 +3,7 @@ $key = "mbctoilet";
 $pass = $_GET['pass'];
 
 $encpass = openssl_encrypt($pass,'AES-128-ECB',$key);
+$decpass = openssl_decrypt($encpass,'AES-128-ECB',$key);
 
-var_dump($encpass);
+var_dump($encpass,$decpass);
 ?>
