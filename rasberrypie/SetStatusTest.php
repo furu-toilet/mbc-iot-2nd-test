a0ba55b466d2a872c6c675b5f143570d9c38d293
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-06-27 14:07:44 +0900

    Update SetStatusTest.php

--- rasberrypie/SetStatusTest.php
+++ rasberrypie/SetStatusTest.php
@@ -55,7 +55,7 @@ if($flg == true){
     $sql = 'SELECT "Status" 
             FROM "ToiletTerminal";';      //DBManagerからSQL文が決まったらここに入力！   現在のトイレの状態を取得するクエリ（実行結果：-1,0,1　のどれか）
     $NowStatus = $db->db_sql($sql);    //現在のトイレの情報を取得
-    if($NowStatus !== 1){   //在室ならDBを操作しない（誤作動の可能性を考慮）
+    if($NowStatus !== $status){   //在室ならDBを操作しない（誤作動の可能性を考慮）
         $sql = 'UPDATE "ToiletTerminal" SET 
                        "Status" =  '. $status .',"UpdateTime"    = CURRENT_TimeStamp + \'9 hours\';';
         $db->db_sql($sql);    //状態のセット実行
