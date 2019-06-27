0a62542a44f5c55432d8ad70fff286e5709e903c
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-06-27 11:40:52 +0900

    Update pwhash.php

--- rasberrypie/pwhash.php
+++ rasberrypie/pwhash.php
@@ -1,8 +1,7 @@
 <?php
 $pass = $_GET['pass'];
 
-//$hash = password_hash($password, PASSWORD_BCRYPT);
-$hash = password_hash($password);
+$hash = password_hash($password, PASSWORD_BCRYPT);
 
 var_dump($hash);
 ?>
