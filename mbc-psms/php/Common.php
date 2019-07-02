89d0e8fb4ebe91e7b77a301bcd7b5c7b88fcb221
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-07-02 13:56:55 +0900

    Update Common.php

--- mbc-psms/php/Common.php
+++ mbc-psms/php/Common.php
@@ -64,7 +64,8 @@ function db_sql($sql){
   	foreach(sql_split($mltsql) as $ex_sql){
 		array_push($ex_result,sql_once($ex_sql));
 	}*/
-	$ex_result = ["sql" => "123", "data" => "456"];
+	//$ex_result = ["sql" => "123", "data" => "456"];
+	sql_split("select * from user_info;");
   	return $ex_result;
   }
   /* SQL分割用メソッド(文字列) （未完）*/
