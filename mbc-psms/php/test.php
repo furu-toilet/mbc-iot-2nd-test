85e41413022a6868b194891e1a17c227c36772f2
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-07-03 14:34:41 +0900

    Update test.php

--- mbc-psms/php/test.php
+++ mbc-psms/php/test.php
@@ -1,10 +1,36 @@
 <?php
+/*
 require_once "./Common.php";
 $db = new Common();
 
 $sql = "select * from user_info;";
+*/
 
-var_dump($db->sql_excute($sql));
+function sql_split($mltsql){
+	$split_arr = array();
+	$start = 0;
+	while(true){
+	    $leng = mb_strlen($mltsql);
+	    $vis = mb_strpos($mltsql, ';', $start, UTF-8);
+	    array_push($split_arr,mb_substr($mltsql,$start,$vis));
+	    $start = $vis + 1;
+	    $mltsql = mb_substr($mltsql,$start,$leng);
+	    if($mltsql < 2 || mb_strpos($mltsql, ';', $start, UTF-8) == false){
+	    	break;
+	    }
+	}
+	return $split_arr;
+}
+
+$datalist = 
+  '
+  select * from user_info;
+  select * from "RuiInfo";
+  select * from "ToiletTerminal";
+  select * from abctable;
+  ';
+
+$result = sql_split($datalist);
 
 
 ?>
