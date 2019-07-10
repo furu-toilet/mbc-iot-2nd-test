55edff99d06783a91c3381af78f3e3a092d5dd6c
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-07-10 13:33:01 +0900

    Update test.php

--- mbc-psms/php/test.php
+++ mbc-psms/php/test.php
@@ -7,19 +7,19 @@ $sql = "select * from user_info;";
 */
 
 function sql_split($mltsql){
-	//$split_arr = array();
-	//$start = 0;
-	//while(true){
-	    //$leng = mb_strlen($mltsql);
-	    //$vis = mb_strpos($mltsql, ";");
-	    //array_push($split_arr,mb_substr($mltsql,$start,$vis));
-	    //$start = $vis + 1;
-	    //$mltsql = mb_substr($mltsql,$start,$leng);
-	    //if($mltsql < 2 || mb_strpos($mltsql, ';', $start, UTF-8) == false){
-	    //	break;
-	    //}
-	//}
-	//return $split_arr;
+	$split_arr = array();
+	$start = 0;
+	while(true){
+	    $leng = strlen($mltsql);
+	    $vis = strpos($mltsql, ";");
+	    array_push($split_arr,substr($mltsql,$start,$vis));
+	    $start = $vis + 1;
+	    $mltsql = substr($mltsql,$start,$leng);
+	    if($mltsql < 2 || strpos($mltsql, ';', $start, UTF-8) == false){
+	    	break;
+	    }
+	}
+	return $split_arr;
 }
 
 $datalist = 
@@ -30,15 +30,8 @@ $datalist =
   select * from abctable;
   ';
 
-//sql_split($datalist);
+var_dump( sql_split($datalist) );
 
-//$str = ";";
-//var_dump( mb_strpos($datalist, "s") );
-
-$haystack = "日本語の文字列を検索。日本語検索。";
-var_dump(mb_strpos($haystack, "文"));
-var_dump(mb_strpos($haystack, "検"));
-var_dump(mb_strpos($haystack, "国"));
 
 
 
