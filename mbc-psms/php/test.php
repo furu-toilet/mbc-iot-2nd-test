2edc3b6c7f29d3c5e44aaee7657a31bbd17141f8
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-07-10 14:09:48 +0900

    Update test.php

--- mbc-psms/php/test.php
+++ mbc-psms/php/test.php
@@ -1,24 +1,7 @@
 <?php
-/*
+
 require_once "./Common.php";
 $db = new Common();
-
-$sql = "select * from user_info;";
-*/
-
-function sql_split($mltsql){
-	$split_arr = array();
-	$start = 0;
-	while(true){
-	    $vis = strpos($mltsql, ";", $start) + 1;
-	    array_push($split_arr,substr($mltsql,$start,$vis - $start));
-	    $start = $vis;
-	    if(strpos($mltsql, ";", $start) == false){
-	    	break;
-	    }
-	}
-	return $split_arr;
-}
 
 $datalist = 
   '
@@ -28,14 +11,7 @@ $datalist =
   select * from abctable;
   ';
 
-var_dump( sql_split($datalist) );
-
-$data2 = "a;b;c;d;e;f;g;";
-
-//var_dump( sql_split($data2) );
-
-//$vis = $start - strpos($mltsql, ";", 6) + 1;
-//var_dump(substr($data2,6,$vis));
+var_dump( $db->sql_excute($datalist) );
 
 
 ?>
