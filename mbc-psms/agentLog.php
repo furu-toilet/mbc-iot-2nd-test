71c9c76c3c97b79d07eb2b3605f68c83fcdf34d3
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-08-06 16:46:54 +0900

    Update agentLog.php

--- mbc-psms/agentLog.php
+++ mbc-psms/agentLog.php
@@ -1,7 +1,8 @@
 <?php
 
 /* 以下テスト */
-$agentLog = $_SERVER['HTTP_USER_AGENT'];
+//$agentLog = $_SERVER['HTTP_USER_AGENT'];
+$agentLog = $_SERVER['HTTP_X_FORWARDED_FOR']
 var_dump($agentLog);
 /* 以上テスト */
 
