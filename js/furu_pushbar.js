a0482b6471dde46115b96df1b295759b230ea390
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-08-23 11:28:12 +0900

    Create furu_pushbar.js

---
+++ js/furu_pushbar.js
@@ -0,0 +1,9 @@
+Push.create("Push 通知だよ！", {
+    body: "本文だよここに表示されるよ",
+    icon: './images/icon.png', // 右側に表示される画像のパス
+    timeout: 4000,
+    onClick: function () {
+        location.href = 'https://yahoo.co.jp';
+        this.close();
+    }
+});
