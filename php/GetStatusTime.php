bc7e6e452cd8df0680cc2c0403461166d194f98c
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-08-23 16:52:11 +0900

    Update GetStatusTime.php

--- php/GetStatusTime.php
+++ php/GetStatusTime.php
@@ -1,10 +1,22 @@
 <?php
+/* このファイルについて */
+// 作成者：古澤
+// 最終更新日付：2019/08/23
+// 戻り値：DBの値を配列で返す。ToiletTerminalテーブル
+//        array(status,UpdateTime)
 
-require_once "./../Common.php";
 
+/* 以下コード */
+require_once "./Common.php";
+$db = new Common();
 
+$sql = "select * from \"ToiletTerminal\" where \"TanmatsuInfo\" = 'ラズパイ1F男子トイレ';";
+$SqlData = $db->db_sql($sql);     // 現在１レコードを想定
 
+$StatusData = $SqlData['status'];
+$UpdateTimeData = $SqlData['UpdateTime'];
 
+$result = json_encode(array("status"=>$StatusData,"UpdateTime"=>$UpdateTimeData));
 
-
+echo $result;
 ?>
