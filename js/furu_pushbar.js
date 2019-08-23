26ee434163079777057080814c9a6e35db4e68d8
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-08-23 11:36:22 +0900

    Update furu_pushbar.js

--- js/furu_pushbar.js
+++ js/furu_pushbar.js
@@ -1,9 +1,11 @@
-Push.create("Push 通知だよ！", {
-    body: "本文だよここに表示されるよ",
-    icon: './images/icon.png', // 右側に表示される画像のパス
-    timeout: 4000,
-    onClick: function () {
-        location.href = 'https://yahoo.co.jp';
-        this.close();
-    }
-});
+function GoPushbar(){    
+    Push.create("Push 通知だよ！", {
+        body: "本文だよここに表示されるよ",
+        icon: './images/icon.png', // 右側に表示される画像のパス
+        timeout: 4000,
+        onClick: function () {
+            location.href = 'https://yahoo.co.jp';
+            this.close();
+        }
+    });
+}
