c3a1cc2ecdf526b170990d2b3763aa7f4b0fe2bb
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-08-30 11:33:56 +0900

    Update OnchangeTest.js

--- js/OnchangeTest.js
+++ js/OnchangeTest.js
@@ -2,7 +2,9 @@ var OnStatus = document.getElementById('vacancy');
 
 //OnStatus.onchange = EveDoing();
 
-OnStatus.addEventListener('change',EveDoing());
+OnStatus.addEventListener('change',function(){
+  EveDoing();
+});
 
 function EveDoing(){
   console.log("function start");
