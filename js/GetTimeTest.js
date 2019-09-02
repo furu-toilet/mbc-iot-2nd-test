ef83d22efefcc1c688c8e660596b1df1234308b6
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-09-02 14:06:30 +0900

    Update and rename OnchangeTest.js to GetTimeTest.js

---
+++ js/GetTimeTest.js
@@ -0,0 +1,24 @@
+var GetMin = null;
+var GetHour = null;
+
+
+function GetTimeStatus(url){
+  return new Promise((resolve,reject) => {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+      if(xhr.readyState ===4 && xhr.status === 200){    //通信が正常時
+        //NowStatus = JSON.parse(xhr.responseText)['Status'];
+        //UpdateTime = JSON.parse(xhr.responseText)['UpdateTime'];
+          //resolve(responsedata);
+        resolve(JSON.parse(xhr.responseText));
+      }else if(xhr.status === 404){                     //通信が異常時
+        console.log("Err : Not Found Code:404");
+        //reject("Err : Not Found");
+      }
+    }    
+    xhr.open("GET",url,true);
+    xhr.send(null);
+  });
+}
+
+console.log(GetTimeStatus("./GetTimeStatus.php"));
--- js/OnchangeTest.js
+++
@@ -1,9 +0,0 @@
-var OnStatus = document.getElementById('vacancy');
-
-//OnStatus.onchange = EveDoing();
-
-//OnStatus.onchange = EveDoing;
-
-var EveDoing = function(){
-  console.log("function start");
-};
