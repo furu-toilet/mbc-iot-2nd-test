e15bf79da3a9bcde3c5edbd43695aa80a7dd501d
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-09-02 14:40:43 +0900

    Update GetTimeTest.js

--- js/GetTimeTest.js
+++ js/GetTimeTest.js
@@ -1,27 +1,24 @@
 var GetMin = null;
 var GetHour = null;
 
-
-//function GetTimeStatus(){
-  GetTimeStatus = new Promise((resolve,reject) => {
-    var url = "./php/GetStatusTime.php";
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
-      if(xhr.readyState ===4 && xhr.status === 200){    //通信が正常時
-        //NowStatus = JSON.parse(xhr.responseText)['Status'];
-        //UpdateTime = JSON.parse(xhr.responseText)['UpdateTime'];
-          //resolve(responsedata);
-        resolve(JSON.parse(xhr.responseText));
-        return JSON.parse(xhr.responseText);
-      }else if(xhr.status === 404){                     //通信が異常時
-        console.log("Err : Not Found Code:404");
-        //reject("Err : Not Found");
-      }
-    }    
-    xhr.open("GET",url,true);
-    xhr.send(null);
-  });
-//}
+GetTimeStatus = new Promise((resolve,reject) => {
+  var url = "./php/GetStatusTime.php";
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState ===4 && xhr.status === 200){    //通信が正常時
+      //NowStatus = JSON.parse(xhr.responseText)['Status'];
+      //UpdateTime = JSON.parse(xhr.responseText)['UpdateTime'];
+        //resolve(responsedata);
+      resolve(JSON.parse(xhr.responseText));
+      return JSON.parse(xhr.responseText);
+    }else if(xhr.status === 404){                     //通信が異常時
+      console.log("Err : Not Found Code:404");
+      //reject("Err : Not Found");
+    }
+  }    
+  xhr.open("GET",url,true);
+  xhr.send(null);
+});
 
 GetTimeStatus.then( (data) => {
   console.log(data);
