7ae5c2f05e2a8634aa6746e1208f1478845b97e0
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-09-02 14:31:01 +0900

    Update GetTimeTest.js

--- js/GetTimeTest.js
+++ js/GetTimeTest.js
@@ -3,23 +3,25 @@ var GetHour = null;
 
 
 function GetTimeStatus(url){
-  //return new Promise((resolve,reject) => {
+  return new Promise((resolve,reject) => {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
       if(xhr.readyState ===4 && xhr.status === 200){    //通信が正常時
         //NowStatus = JSON.parse(xhr.responseText)['Status'];
         //UpdateTime = JSON.parse(xhr.responseText)['UpdateTime'];
           //resolve(responsedata);
-        //resolve(JSON.parse(xhr.responseText));
+        resolve(JSON.parse(xhr.responseText));
         return JSON.parse(xhr.responseText);
       }else if(xhr.status === 404){                     //通信が異常時
         console.log("Err : Not Found Code:404");
         //reject("Err : Not Found");
       }
     }    
-    //xhr.open("GET",url,true);
-    //xhr.send(null);
-  //});
+    xhr.open("GET",url,true);
+    xhr.send(null);
+  });
 }
 
-console.log(GetTimeStatus("./php/GetStatusTime.php"));
+GetTimeStatus.then( (data) => {
+  console.log(data);
+});
